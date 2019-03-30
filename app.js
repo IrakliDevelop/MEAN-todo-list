e4d8256fedd8f33b0ada7ed27126262a3fe283a7
biazigaskda <i_iremashvili@cu.edu.ge>
2019-03-30 14:21:43 +0400

    delete build files. update gitignore

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const   express     = require("express"),
-        app         = express(),
-        mongoose    = require("mongoose"),
-        bodyParser  = require("body-parser"),
-        Todo        = require("./models/todo"),
-        cors        = require("cors"),
-        path        = require("path");
+const express = require("express"),
+    app = express(),
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser"),
+    Todo = require("./models/todo"),
+    cors = require("cors"),
+    path = require("path");
 
 
 //mongoose setup
-mongoose.connect("mongodb://localhost/todo_ang_node",(err)=>{
+mongoose.connect("mongodb://localhost/todo_ang_node", (err) => {
     if (err) console.log(err);
 });
 
@@ -18,19 +18,19 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(cors());
 
 //body parser setup
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 //landing
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.send("working");
 });
 
 
 //get todos
-app.get("/todos", (req,res) => {
-    Todo.find((err,todos)=>{
-        if (err){
+app.get("/todos", (req, res) => {
+    Todo.find((err, todos) => {
+        if (err) {
             console.log(err);
         } else {
             res.json(todos);
@@ -40,13 +40,13 @@ app.get("/todos", (req,res) => {
 
 
 //create new todo
-app.post("/todos", (req,res) => {
+app.post("/todos", (req, res) => {
     const newTodo = new Todo({
         text: req.body.text
     });
 
-    Todo.create(newTodo, (err,the_todo) =>{
-        if (err){
+    Todo.create(newTodo, (err, the_todo) => {
+        if (err) {
             console.log(err);
         } else {
             res.json(the_todo);
@@ -54,26 +54,22 @@ app.post("/todos", (req,res) => {
     })
 });
 
-app.delete("/todos/", (req,res)=>{
+app.delete("/todos/", (req, res) => {
     const id = req.body._id;
     console.log(id)
-    Todo.findOneAndDelete({"_id":id}, (err,deleted)=>{
-        if (err){
+    Todo.findOneAndDelete({"_id": id}, (err, deleted) => {
+        if (err) {
             throw err;
         } else {
             res.json(deleted);
         }
     })
-})
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-  });
+});
 
 
 
 const port = process.env.PORT || 3000
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server has started at port ${port}`)
 })
